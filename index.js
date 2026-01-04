bafea9bed25c48a58a236d42275b86a6cae84a99
bakari <anbphones@icloud.com>
2026-01-04 21:48:00 +0300

    analyzing data new

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 
+require('datejs');
 
+function combineUsers(...args) {
+    // Initialize return object
+    let combinedObject = {
+        users: []
+    };
+
+    // Loop through args to merge arrays into users
+    for (let arr of args) {
+        combinedObject.users.push(...arr);
+    }
+
+    // Get today's date and format it
+    let today = new Date();
+    combinedObject.merge_date = today.toString("M/d/yyyy");
+
+    // Return the object
+    return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
